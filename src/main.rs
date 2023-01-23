4e0aeb64a49c1fec73126f8b1d39dee30a45d858
unconv <unconventionalcoding@gmail.com>
2023-01-23 04:08:43 +0200

    switched to using anyhow error handling fixed converting private key multiple times switched to undeprecated base64 decode switched to fs::read_to_string shorthand changed Encryption struct to just a decrypt function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use openssl::pkey::PKey;
-use openssl::pkey::Private;
-use base64;
-use std::fs::File;
-use std::io::{Error, Read};
-extern crate gtk;
-
+use base64::Engine;
+use std::fs;
+use anyhow::{anyhow, Result};
 use gtk::prelude::*;
 use gtk::{FileChooserButton, Entry, Button};
 
@@ -37,64 +34,47 @@ fn build_gui() {
     });
 
     decrypt_button.connect_clicked(move |_| {
-        let encryption = Encryption{};
-        match file_input.filename() {
-            Some(private_key_path) => {
-                let private_key = read_private_key(private_key_path.to_str().unwrap()).unwrap();
-                let private_key_pem = private_key.private_key_to_pem_pkcs8().unwrap();
-                let private_key_pem_str = match std::str::from_utf8(&private_key_pem) {
-                    Ok(v) => v,
-                    Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-                };
-                let encrypted_data = text_input.text();
-                let encrypted_data_str = encrypted_data.as_str();
-                match encryption.decrypt(encrypted_data_str, &private_key_pem_str) {
-                    Ok(decrypted_data) => {
-                        let decrypted_data_string = String::from_utf8(decrypted_data).expect("Data is not a valid utf8 string");
-                        let decrypted_data_string = decrypted_data_string.trim_matches(char::from(0));
-                        // Update the text input with the decrypted data
-                        text_input.set_text(&decrypted_data_string);
-                    },
-                    Err(_e) => {
-                        // Handle the error
-                        text_input.set_text("<unable to decrypt>");
-                    }
+        match decrypt_from_gui(&file_input, &text_input) {
+            Ok(decrypted_data) => text_input.set_text(&decrypted_data),
+            Err(e) => {
+                if e.to_string() == "No keyfile selected" {
+                    text_input.set_text("<please select keyfile>");
+                } else {
+                    text_input.set_text("<unable to decrypt>");
                 }
-            }
-            None => {
-                text_input.set_text("<please select key file>");
             }
         }
     });
-    
 
     gtk::main();
 }
 
-fn read_private_key(path: &str) -> Result<PKey<Private>, Error> {
-    let mut file = File::open(path)?;
-    let mut private_key = String::new();
-    file.read_to_string(&mut private_key)?;
-
-    let pkey = PKey::private_key_from_pem(private_key.as_bytes())?;
-    Ok(pkey)
-}
-
-pub struct Encryption;
-
-impl Encryption {
-    pub fn decrypt(&self, data: &str, private_key: &str) -> Result<Vec<u8>, openssl::error::ErrorStack> {
-        let trimmed_data = data.trim();
-        let decoded_data = match base64::decode(trimmed_data) {
-            Ok(v) => v,
-            Err(_) => "".as_bytes().to_vec(),
-        };
-        let private_key = PKey::private_key_from_pem(private_key.as_bytes())?.rsa()?;
-        let mut decrypted_data = vec![0; private_key.size() as usize];
-        private_key.private_decrypt(&decoded_data, &mut decrypted_data, openssl::rsa::Padding::PKCS1)?;
-        Ok(decrypted_data)
+fn decrypt_from_gui(file_input: &FileChooserButton, text_input: &Entry) -> Result<String, anyhow::Error> {
+    match file_input.filename() {
+        Some(private_key_path) => {
+            let private_key = fs::read_to_string(private_key_path)?;
+            let encrypted_data = text_input.text();
+            let decrypted_data = decrypt(encrypted_data.as_str(), &private_key)?;
+            Ok(decrypted_data)
+        },
+        None => {
+            Err(anyhow!("No keyfile selected"))
+        }
     }
 }
+
+pub fn decrypt(data: &str, private_key: &str) -> Result<String, anyhow::Error> {
+    let decoded_data = base64::engine::general_purpose::STANDARD.decode(data.trim())?;
+
+    let pkey = PKey::private_key_from_pem(private_key.as_bytes())?.rsa()?;
+    let mut decrypted_data = vec![0; pkey.size() as usize];
+    pkey.private_decrypt(&decoded_data, &mut decrypted_data, openssl::rsa::Padding::PKCS1)?;
+
+    let decrypted_data_string = String::from_utf8(decrypted_data)?.trim_matches(char::from(0)).to_string();
+
+    Ok(decrypted_data_string)
+}
+
 fn main() {
     build_gui();
 }
